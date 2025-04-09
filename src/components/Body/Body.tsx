821b25c00ac18d1a102060dc147ba5beabe0d9eb
João Pedro Barbosa Duarte Ferreira <141972472+CronusLDX@users.noreply.github.com>
2025-04-09 19:11:26 -0300

    docs: Adicionando comentrios de especificações nos componentes Body e Heading

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react';
 import styles from './Body.module.css';
-
+// props genericas para alta flexibilidade
 const Body = (props: { children: ReactNode }) => {
   return (
     <div className={styles.heading}>
--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './Heading.module.css';
+
+// props explicitas para tipagem e uso padronizado
 const Heading: React.FC<{ title: string; description: string }> = ({
   title,
   description,
